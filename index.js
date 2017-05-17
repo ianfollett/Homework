027bb45955d9e3877478308342c86dc69173cc79
Ian Follett <ianfollett@gmail.com>
2017-05-16 23:27:14 -0700

    Homework 5 compliant.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 'use strict';
 
-var express = require("express");
-var movies = require("./lib/movies.js");
+let express = require("express");
+let movies = require("./lib/movies.js");
 
 
-var app = express();
+let app = express();
 
 app.use(express.static(__dirname + '/public')); // set location for static files
 
 app.use(require("body-parser").urlencoded({extended: true})); // parse form submissions
 
 
-var handlebars = require("express-handlebars")
+let handlebars = require("express-handlebars")
     .create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
@@ -20,7 +20,8 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', function(req, res) { res.render('home');
+app.get('/', function (req, res) {
+    res.render('home', {movies: movies.getAll()});
 });
 
 app.get('/about', function(req, res) {
@@ -28,33 +29,41 @@ app.get('/about', function(req, res) {
 });
 
 
-app.post('/get', function (req, res) {
-    console.log(req.body);
-    var found = movies.get(req.body.title);
-    res.render("details", {title: req.body.title, result: found});
+app.get('/get', function (req, res) {
+    console.log(req.query);
+    let result;
+    result = movies.get(req.query.title);
+    res.render("details", {title: req.query.title, result: result});
 });
 
 app.get('/delete', function (req, res) {
-    let result = movies.delete(req.query.title);
+    console.log(req.query);
+    let result;
+    result = movies.delete(req.query.title);
     res.render('delete', {title: req.query.title, result: result});
 });
 
 app.post('/add', function (req, res) {
+    console.log(req.body);
     res.type('text/html');
-    var newMovie = {
+    let newMovie;
+    newMovie = {
         "title": req.body.title,
         "year": req.body.year,
         "director": req.body.director,
         "genre": req.body.genre
     };
-    var result = movies.add(newMovie);
+    let result;
+    result = movies.add(newMovie);
     res.render('add', {title: req.body.title, result: result});
 });
+
 
 // Custom 404 page
 app.use(function(req, res, next){ res.status(404);
     res.render('404');
 });
+
 
 // Custom 500 page
 app.use(function(err, req, res, next){ console.error(err.stack);
